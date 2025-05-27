99fe03acf071e639dd2cee86263a433b538ed4b2
Max Allen <maxallen03919@gmail.com>
2025-05-27 10:07:36 +0900

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,7 +10,6 @@ const DEPLOYER_PRIVATE_KEY =
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
-
   for (const account of accounts) {
     console.log(account.address);
   }
@@ -19,9 +18,6 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
   solidity: "0.8.9",
   networks: {
